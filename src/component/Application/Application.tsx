18ac2185ef0ce194ea8c47e4c94343727fe12834
nekko1119 <taku50501119@gmail.com>
2018-05-06 03:04:24 +0900

    Update typescript

--- src/component/Application/Application.tsx
+++ src/component/Application/Application.tsx
@@ -2,11 +2,11 @@ import * as cx from "classnames";
 import * as React from "react";
 import { main } from "../main.css";
 
-const Application = () => (
+const Application = ({ match }: { match: { url: string; }}) => (
   <div className={cx("container", main)}>
     <h1 className="mx-5 py-5">適当に作ったもの</h1>
     <ul>
-      <li><a href="/app/stage-logo-maker">画像にダクソっぽく文字乗せる</a></li>
+      <li><a href={`${match.url}/stage-logo-maker`}>画像にダクソっぽく文字乗せる</a></li>
     </ul>
   </div>
 );
